563bf52e3e7ce20c71d25e0108c5f05c2c54cbb9
Prachi Yaragatti <yaragattiprachi@gmail.com>
2026-01-01 10:14:36 +0530

    routes: video.js updated

--- Backend/routes/video.js
+++ Backend/routes/video.js
@@ -1,106 +1,60 @@
-const express = require('express')
-const pool = require('../db/pool')
-const result = require('../utils/result')
-const auth = require('../utils/auth')
+const express = require("express") // js framework, used create backend server app
+const result = require("../utils/result")
+const pool = require("../db/pool")
+const { checkAuthorization } = require("../utils/auth")
+const router = express.Router() // mini express app, for handling routes, router instance
 
-const router = express.Router()
+// Admin APIs
+// GET : /video/all-videos?course_id=?
+router.get("/all-videos", checkAuthorization, (req, res) => {
+    const { course_id } = req.query;
 
-// GET /video/all-videos
-router.get('/all-videos', (req, res) => {
+    let sql = `
+        SELECT v.video_id, v.title, v.description, v.youtube_url, v.course_id, c.course_name
+        FROM videos v
+        JOIN courses c ON v.course_id = c.course_id
+    `;
 
-  const { course_id } = req.query
+    const params = [];
 
-  let sql = `
-    SELECT 
-      video_id,
-      course_id,
-      title,
-      description,
-      youtube_url,
-      added_at
-    FROM videos
-  `
+    if (course_id) {
+        sql += " WHERE v.course_id = ?";
+        params.push(course_id);
+    }
 
-  let params = []
+    pool.query(sql, params, (error, data) => {
+        res.send(result.createResult(error, data));
+    });
+});
 
-
-  if (course_id) {
-    sql += ` WHERE course_id = ?`
-    params.push(course_id)
-  }
-
-  pool.query(sql, params, (error, data) => {
-    res.send(result.createResult(error, data))
-  })
-})
 
 // POST : /video/add
-router.post('/add', auth.authAdmin, (req, res) => {
-
-  const {
-    course_id,
-    title,
-    youtube_url,
-    description
-  } = req.body
-
-  const sql = `
-    INSERT INTO videos
-    (course_id, title, youtube_url, description, added_at)
-    VALUES (?, ?, ?, ?, CURDATE())
-  `
-
-  pool.query(
-    sql,
-    [course_id, title, youtube_url, description],
-    (error, data) => {
-      res.send(result.createResult(error, data))
-    }
-  )
+router.post("/add", checkAuthorization, (req,res) => {
+    const {course_id, title, description, youtube_url} = req.body
+    const sql = `INSERT INTO videos(course_id, title, description, youtube_url) VALUES(?,?,?,?);`
+    pool.query(sql, [course_id, title, description, youtube_url], (error, data) => {
+        res.send(result.createResult(error,data))
+    })
 })
 
-// PUT /video/update/:video_id (ADMIN)
-router.put('/update/:video_id', auth.authAdmin, (req, res) => {
-
-  const { video_id } = req.params
-  const {
-    course_id,
-    title,
-    youtube_url,
-    description
-  } = req.body
-
-  const sql = `
-    UPDATE videos
-    SET
-      course_id = ?,
-      title = ?,
-      youtube_url = ?,
-      description = ?
-    WHERE video_id = ?
-  `
-
-  pool.query(
-    sql,
-    [course_id, title, youtube_url, description, video_id],
-    (error, data) => {
-      res.send(result.createResult(error, data))
-    }
-  )
+// PUT : /video/update/:video_id
+router.put("/update/:video_id", checkAuthorization, (req,res) => {
+    const {video_id} = req.params
+    const {course_id, title, description, youtube_url} = req.body
+    const sql = `UPDATE videos SET course_id = ?, title = ?, description = ?, youtube_url = ? WHERE video_id=?`
+    pool.query(sql, [course_id, title, description, youtube_url, video_id], (error, data) => {
+        res.send(result.createResult(error,data))
+    })
 })
 
-// DELETE /video/delete/:video_id (ADMIN)
-router.delete('/delete/:video_id', auth.authAdmin, (req, res) => {
-
-  const { video_id } = req.params
-
-  const sql = `DELETE FROM videos WHERE video_id = ?`
-
-  pool.query(sql, [video_id], (error, data) => {
-    res.send(result.createResult(error, data))
-  })
+// DELETE : /video/delete/:video_id
+router.delete("/delete/:video_id", checkAuthorization, (req,res) => {
+    const {video_id} = req.params
+    const sql = `DELETE FROM videos WHERE video_id=?;`
+    pool.query(sql, [video_id], (error, data) => {
+        res.send(result.createResult(error,data))
+    })
 })
-
-
 
 module.exports = router
+
